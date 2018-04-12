f293f3c1f7335b71657baa7efd2f776b339865ed
Lucas Tadeu <30877738+lucastg@users.noreply.github.com>
2018-04-12 20:16:31 -0300

    Update TelaVeiculo.java

--- src/aep3/TelaVeiculo.java
+++ src/aep3/TelaVeiculo.java
@@ -3,13 +3,5 @@ package aep3;
 import java.util.Scanner;
 
 public class TelaVeiculo {
-    
-    Scanner scan = new Scanner(System.in);
-    public int menu(){
-        System.out.println("______Menu______");
-        System.out.println("0 - opcoes");
-        int opcao = scan.nextInt();
-        return opcao;
-    }
-    
+
 }
